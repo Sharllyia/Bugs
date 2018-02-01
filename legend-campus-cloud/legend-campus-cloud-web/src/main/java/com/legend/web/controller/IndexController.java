34c7ddf9798532292cdc94d4f211d0889ca529de
Hp <35661942+hupeiD@users.noreply.github.com>
2018-02-01 19:10:13 +0800

    legend-campus-cloud update

---
+++ legend-campus-cloud/legend-campus-cloud-web/src/main/java/com/legend/web/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.legend.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * @author Administrator
+ * @date 2018/2/1
+ */
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+}
