d0da256761ecb9574d2e09d2501d9503fec96cfe
Hp <35661942+hupeiD@users.noreply.github.com>
2018-02-13 13:55:52 +0800

    legend-module update

--- legend-module/legend-base/legend-base-dao/src/main/java/com/legend/base/dao/mapper/BaseBannerMapper.java
+++ legend-module/legend-base/legend-base-dao/src/main/java/com/legend/base/dao/mapper/BaseBannerMapper.java
@@ -4,6 +4,9 @@ import com.legend.base.dao.BaseMapper;
 import com.legend.base.entity.BaseBanner;
 import com.legend.base.entity.BaseBannerExample;
 
+/**
+ * @author Administrator
+ */
 public interface BaseBannerMapper extends BaseMapper<BaseBanner> {
     int countByExample(BaseBannerExample example);
 }
