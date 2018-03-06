941093292e8d767d80aec464677d4f82cc198782
Hp <35661942+hupeiD@users.noreply.github.com>
2018-03-06 20:15:23 +0800

    legend-base-web

---
+++ legend-module/legend-base/legend-base-web/src/main/java/com/legend/module/base/web/LegendBaseWebApplication.java
@@ -0,0 +1,12 @@
+package com.legend.module.base.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendBaseWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendBaseWebApplication.class, args);
+	}
+}
